4cf6e2f6dfccfdcb095fa9429e565c206a3af6d9
Dominique DERRIER <derrierdo@gmail.com>
2014-05-03 19:48:06 -0400

    Adding test

--- Generate.sh
+++ Generate.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-#DISK=
-#MOUNT=/mnt/disk
+# fdisk to create one partition
+# mkfs.vfat -F32 -n BOOTER /dev/sdX1
 
-NAME=BOOTER
+if [  "x$1" = "x" ]
+then
+echo "Need a key name"
+fi
+
+NAME=$1
 DEST=$(mount |grep $NAME | awk  	 '{print $3}')
 DISK=$(mount |grep $NAME | awk -F'[0-9]' '{print $1}')
+echo "Please check !"
 echo "Transform $DISK $NAME en clef bootable $DEST" 
 read
 
+##########################
 # Get distrib
 ########
 mkdir -p tmp
@@ -23,10 +30,15 @@ wget -c 'http://downloads.sourceforge.net/project/dban/dban/dban-2.2.8/dban-2.2.
 wget -c 'http://distro.ibiblio.org/tinycorelinux/5.x/x86/release/TinyCore-current.iso' -O tinycore.iso
 #Gentoo
 wget -c 'http://distfiles.gentoo.org/releases/x86/autobuilds/current-install-x86-minimal/install-x86-minimal-20140415.iso' -O gentoo.iso
-cp *.iso ${DEST}
+cp -v *.iso ${DEST}
 popd
 
 # Grub
 ########
+if [ `which grub2-install`  ]
+then
 grub2-install --boot-directory=${DEST} ${DISK}
 cp grub.cfg ${DEST}/grub
+else
+echo "Grub not found :'"
+fi
