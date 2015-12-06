b90b8fef6a1380b882797b3a104f2ccd8c6147a2
Dominique DERRIER <derrierdo@gmail.com>
2015-12-06 10:41:19 -0500

    Clean Generate

--- Generate.sh
+++ Generate.sh
@@ -20,25 +20,6 @@ read
 ##########################
 # Get distrib
 ########
-function Download()
-{
-mkdir -p tmp
-pushd tmp
-#Debian 
-wget -c 'http://ftp.ca.debian.org/debian/dists/testing/main/installer-i386/current/images/netboot/mini.iso' -O mini.iso
-##DamnSmall Linux
-wget -c 'http://distro.ibiblio.org/damnsmall/current/dsl-4.4.10-initrd.iso' -O dsl.iso
-##DBAN
-wget -c 'http://downloads.sourceforge.net/project/dban/dban/dban-2.2.8/dban-2.2.8_i586.iso?r=http%3A%2F%2Fwww.dban.org%2Fdownload&ts=1399148236&use_mirror=hivelocity' -O dban.iso
-##TinyCore
-wget -c 'http://distro.ibiblio.org/tinycorelinux/5.x/x86/release/TinyCore-current.iso' -O tinycore.iso
-##Gentoo
-wget -c 'http://distfiles.gentoo.org/releases/x86/autobuilds/current-install-x86-minimal/install-x86-minimal-20141209.iso' -O gentoo.iso
-wget -c 'http://www.memtest.org/download/5.01/memtest86+-5.01.bin.gz' -O memtest86.bin.gz
-zcat memtest86.bin.gz > memtest86.bin
-wget -c "http://distro.ibiblio.org/tinycorelinux/6.x/x86_64/release/CorePure64-6.0.iso" -O tinycore64.iso
-popd
-}
 
 
 pushd tmp
